69ec19c51b027d63330fe2c8bd655196730fc319
Diego Fernandes <diego.schell.f@gmail.com>
2017-12-17 22:25:19 -0200

    Animated API

--- App.js
+++ App.js
@@ -1,37 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
+  View,
   StyleSheet,
-  Text,
-  View
+  Animated,
+  Easing,
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const x = new Animated.Value(1);
+const y = new Animated.divide(500, x);
 
 export default class App extends Component<{}> {
+  state = {
+    ballPosX: x,
+    ballPosY: y,
+  }
+
+  componentDidMount() {
+    Animated.timing(this.state.ballPosX, {
+      toValue: 250,
+      duration: 1000,
+    }).start();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Animated.View style={[styles.ball, {
+          marginTop: this.state.ballPosX,
+          marginLeft: this.state.ballPosY,
+        }]} />
       </View>
     );
   }
@@ -40,18 +37,13 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 20,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  ball: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    backgroundColor: '#F00',
   },
 });
